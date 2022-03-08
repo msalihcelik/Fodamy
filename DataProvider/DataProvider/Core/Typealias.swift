37916580e75fb6028b6f094933f3a70ec3938341
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-08 08:59:28 +0300

    add DataProvider base modules

---
+++ DataProvider/DataProvider/Core/Typealias.swift
@@ -0,0 +1,9 @@
+//
+//  Typealias.swift
+//  DataProvider
+//
+//  Created by Mehmet Salih ÇELİK on 8.03.2022.
+//
+
+public typealias RequestParameters = [String: Any]
+public typealias RequestHeaders = [String: String]
