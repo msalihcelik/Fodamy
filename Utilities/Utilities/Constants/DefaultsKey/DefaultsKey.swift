4b5090bdfbc4e3b2af1eaec95c4df9d14792d601
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-10 09:35:26 +0300

    configure mobillium user defaults

---
+++ Utilities/Utilities/Constants/DefaultsKey/DefaultsKey.swift
@@ -0,0 +1,11 @@
+//
+//  DefaultsKey.swift
+//  Utilities
+//
+//  Created by Mehmet Salih ÇELİK on 10.03.2022.
+//
+
+public extension DefaultsKey {
+    static let isWalkThroughCompleted = Key<Bool>(key: "isWalkThroughCompleted")
+    static let userId = Key<Int>(key: "userId")
+}
