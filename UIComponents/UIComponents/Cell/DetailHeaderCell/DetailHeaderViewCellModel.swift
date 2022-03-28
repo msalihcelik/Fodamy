0452ff2a57b4bcac14aabf6a0787e60e01ad1812
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-28 11:05:17 +0300

    add detail header cell

---
+++ UIComponents/UIComponents/Cell/DetailHeaderCell/DetailHeaderViewCellModel.swift
@@ -0,0 +1,32 @@
+//
+//  DetailHeaderViewCellModel.swift
+//  UIComponents
+//
+//  Created by Mehmet Salih ÇELİK on 24.03.2022.
+//
+
+import Foundation
+
+public protocol DetailHeaderViewCellDataSource: AnyObject {
+    var imageUrl: String { get }
+    var isEditorChoice: Bool { get }
+}
+
+public protocol DetailHeaderViewCellEventSource: AnyObject {
+    
+}
+
+public protocol DetailHeaderViewCellProtocol: DetailHeaderViewCellDataSource, DetailHeaderViewCellEventSource {
+    
+}
+
+public final class DetailHeaderViewCellModel: DetailHeaderViewCellProtocol {
+    
+    public var imageUrl: String
+    public var isEditorChoice: Bool
+    
+    public init(imageUrl: String, isEditorChoice: Bool) {
+        self.imageUrl = imageUrl
+        self.isEditorChoice = isEditorChoice
+    }
+}
