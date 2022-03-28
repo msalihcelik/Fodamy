90eff1c2896299c973e4af8c08d069188f5775dd
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-28 11:12:23 +0300

    add comment cell

---
+++ UIComponents/UIComponents/Cell/CommentCell/CommentCellModel.swift
@@ -0,0 +1,71 @@
+//
+//  CommentCellModel.swift
+//  UIComponents
+//
+//  Created by Mehmet Salih ÇELİK on 25.03.2022.
+//
+
+public protocol CommentCellDataSource: AnyObject {
+    var userId: Int { get }
+    var imageUrl: String? { get }
+    var username: String? { get }
+    var recipeAndFollower: String? { get }
+    var time: String? { get }
+    var commentId: Int { get }
+    var commentText: String? { get set }
+    var isOwner: Bool { get set }
+}
+
+public protocol CommentCellEventSource: AnyObject {
+    var ownerButtonTapped: VoidClosure? { get set }
+    var commentChanged: VoidClosure? { get set }
+}
+
+public protocol CommentCellProtocol: CommentCellDataSource, CommentCellEventSource {}
+
+public final class CommentCellModel: CommentCellProtocol {
+    
+    public var ownerButtonTapped: VoidClosure?
+    public var commentChanged: VoidClosure?
+    
+    public var isOwner: Bool
+    
+    public var userId: Int
+    public var imageUrl: String?
+    public var username: String?
+    public var recipeAndFollower: String?
+    public var time: String?
+    public var commentId: Int
+    public var commentText: String?
+
+    public init(userId: Int,
+                imageUrl: String?,
+                username: String?,
+                recipeAndFollower: String?,
+                time: String?,
+                commentId: Int,
+                commentText: String?,
+                isOwner: Bool) {
+        self.userId = userId
+        self.imageUrl = imageUrl
+        self.username = username
+        self.recipeAndFollower = recipeAndFollower
+        self.time = time
+        self.commentId = commentId
+        self.commentText = commentText
+        self.isOwner = isOwner
+    }
+    
+    public convenience init(comment: GetCommentResponse) {
+        let recipeAndFollower = "\(comment.user.recipeCount) \(L10n.Comment.recipe) \(comment.user.followedCount) \(L10n.Comment.follower)"
+        let isOwner = comment.user.id == DefaultsKey.userId.value
+        self.init(userId: comment.user.id,
+                  imageUrl: comment.user.image?.url,
+                  username: comment.user.username,
+                  recipeAndFollower: recipeAndFollower,
+                  time: comment.difference,
+                  commentId: comment.id,
+                  commentText: comment.text,
+                  isOwner: isOwner)
+    }
+}
