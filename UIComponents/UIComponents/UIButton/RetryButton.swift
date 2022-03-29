b9fb06b2c8c73a2e7931e9452a3d62225980f022
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-29 12:40:08 +0300

    add RetryButton in base scene

---
+++ UIComponents/UIComponents/UIButton/RetryButton.swift
@@ -0,0 +1,34 @@
+//
+//  RetryButton.swift
+//  UIComponents
+//
+//  Created by Mehmet Salih ÇELİK on 29.03.2022.
+//
+
+public class RetryButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureContents()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureContents()
+    }
+}
+
+// MARK: - Configure
+extension RetryButton {
+    
+    private func configureContents() {
+        backgroundColor = .appRed
+        tintColor = .appWhite
+        titleLabel?.font = .font(.nunitoBold, size: .xLarge)
+        layer.cornerRadius = 5
+        titleLabel?.textAlignment = .center
+        setTitle(L10n.General.retry, for: .normal)
+        widthAnchor.constraint(equalToConstant: 150).isActive = true
+        heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+}
