0f141563ae84cd6df7918af270af0063232696b8
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-31 06:42:50 +0300

    add validation

---
+++ Fodamy/Validation/Validation.swift
@@ -0,0 +1,38 @@
+//
+//  Validation.swift
+//  Fodamy
+//
+//  Created by Mehmet Salih ÇELİK on 31.03.2022.
+//
+
+class Validation {
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        
+        guard emailPred.evaluate(with: email) else {
+            ToastPresenter.showWarningToast(text: "L10n.Validation.email")
+            return false
+        }
+        return true
+    }
+    
+    func isValidPassword(_ password: String) -> Bool {
+        guard password.count > 5 else {
+            ToastPresenter.showWarningToast(text: "L10n.Validation.password")
+            return false
+        }
+        return true
+    }
+    
+    func isValidName(_ name: String) -> Bool {
+        let regex = "[A-Za-z]{2,}"
+        let test = NSPredicate(format: "SELF MATCHES %@", regex)
+        guard test.evaluate(with: name) else {
+            ToastPresenter.showWarningToast(text: "L10n.Validation.name")
+            return false
+        }
+        return true
+    }
+}
